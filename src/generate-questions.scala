6aa2e6937af661382c5022bac17e198979ed675e
timsumdog <tim@sumdog.com>
2016-01-13 16:04:03 +0000

    Initial commit

---
+++ src/generate-questions.scala
@@ -0,0 +1,7 @@
+object GenerateQuestions {
+  def main(args: Array[String]) {
+    println("3 + 4 = ?; 5, 7, 3, 8")
+    println("5 + 8 = ?; 13, 9, 23, 15")
+    println("47 + 24 = ?; 43, 61, 78, 42")
+  }
+}
